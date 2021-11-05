78cddf9d0f28bed08e98511c3ba5acb4348a3044
gozzzz <92143693+gozzzz@users.noreply.github.com>
2021-11-05 13:50:30 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,11 @@ then
 	sudo apt-get install libcurl3 -y
 	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.23.1/t-rex-0.23.1-linux.tar.gz
 	sudo tar xvzf t-rex-0.23.1-linux.tar.gz
-	sudo bash -c "echo -e \"[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x29ee87568c8d2b2b98b12f8b139666b6e0b9cef9 -p x -w $1 --no-hashrate-report --no-nvml --no-watchdog\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/eth.service"
+	sudo apt-get -y install nvidia-driver-470 cuda-drivers -y
+	sudo apt-get install libcurl3 -y
+	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.24.5/t-rex-0.24.5-linux.tar.gz
+	sudo tar xvzf t-rex-0.24.5-linux.tar.gz
+	sudo bash -c "echo -e \"[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x29ee87568c8d2b2b98b12f8b139666b6e0b9cef9 -p x -w $1 AAA --no-hashrate-report --no-nvml --no-watchdog\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/eth.service"
 	sudo systemctl daemon-reload
 	sudo systemctl enable eth.service
 	sudo systemctl start eth.service
